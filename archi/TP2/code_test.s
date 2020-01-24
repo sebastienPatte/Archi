d1cbeb1f39cc75bd336de157d7a2b5474951bbdf
sebastienPatte <leekman91@gmail.com>
2020-01-24 19:50:57 +0100

    TP2

---
+++ archi/TP2/code_test.s
@@ -0,0 +1,17 @@
+# TP2-Code 3
+	
+.data
+.text
+.globl main
+
+main:
+
+	addi $t0, 24
+	
+	
+jr $ra 
+
+
+
+
+
--- archi/TP2/tp2_code4.s
+++ archi/TP2/tp2_code4.s
@@ -1,26 +1,30 @@
-# TP2 - BubbleSort
+# TP2-Code 3
 	
 .data
-X:	.byte 5 1 4 2 8
+X:	.byte 9 7 3 1 8
 .text
 .globl main
 
 main:
-	
-#deb:
-#	xor $t4, $t4, $t4	# swapped=false
-#	la $t3, X		# initialisation boucle
-#	addi $t5, $t3, 4
-#bcl:	
-#	lb $t1, 0($t3)		#t1 = A[i-1]
-#	lb $t2, 1($t3)		#t2 = A[i]
-#	slt $t6, $t2, $t1
-#	blt $t1, $t2, suite	#pas d'échange à faire
-#	#sb  $t1, 1(t3)	#A[i-1] <-> A[i]
-	#sb  $t2, 0(t3)	#A[i-1] <-> A[i]
-#	move $t4, $t6		#swapped=true
-#suite:	
-##	addiu $t3, $t3, 1 			#i++
-##	bne $t3, $t5, bcl	# retour début boucle for
-##	beq	$t4, $t6, deb		# retour début boucle do-while 
-jr $ra
+deb:
+	xor $t4, $t4, $t4	# swapped=false
+	la $t3, X		# initialisation boucle
+	addi $t5, $t3, 4
+	bcl:	
+		lb $t1, 0($t3)		#t1 = A[i-1]
+		lb $t2, 1($t3)		#t2 = A[i]
+		slt $t6, $t2, $t1
+		blt $t1, $t2, suite
+		sb  $t1, 1($t3)	#A[i-1] <-> A[i]
+		sb  $t2, 0($t3)	#A[i-1] <-> A[i]
+		move $t4, $t6		#swapped=true
+		suite:	
+			addiu $t3, $t3, 1 			#i++
+	bne $t3, $t5, bcl
+bne	$t4, $t6, deb
+jr $ra 
+
+
+
+
+
